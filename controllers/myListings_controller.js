b8b315777ada68c45c51abff3c34d2614ee8ad30
yandrew <yandrew@gmail.com>
2015-03-07 19:33:05 -0800

    adds working table for buyer dashboard. 	-able to order by the header 	-need to order by time 	-need to get working timer left.

--- controllers/myListings_controller.js
+++ controllers/myListings_controller.js
@@ -1,4 +1,9 @@
 app.controller('MyListingsCtrl',function($scope, UserListings) {
+	$scope.orderByField = 'expires_at';
+  $scope.reverseSort = false;
+
   $scope.listings = UserListings.all();
 	UserListings.update();
+
+
 })
