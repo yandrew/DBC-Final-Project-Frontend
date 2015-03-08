47cba5e6d23bc7600ddda6dddbc1c3d4790904c4
yandrew <yandrew@gmail.com>
2015-03-07 23:35:13 -0800

    adding default homepage redirect

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('sellMe',['ui.router','mgcrea.ngStrap']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  // $urlRouterProvider.otherwise('/products');
+  $urlRouterProvider.otherwise('/products');
   $stateProvider
     .state('marketPlace',{
       url: '/marketPlace',
