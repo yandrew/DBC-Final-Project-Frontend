63b606f84c2676510ac8b78b50befcdbcb770db0
yandrew <yandrew@gmail.com>
2015-03-08 18:53:00 -0700

    adds useroffers controller, offers factory, template -not working yet, need to link new js files on index.html

---
+++ services/offers.js
@@ -0,0 +1,29 @@
+app.factory('Offers', function($http, $q, $rootScope) {
+  var urlBase = 'http://localhost:3000';
+  var offers = [];
+  return {
+    update: function(user_id) {
+      console.log(user_id)
+      console.log(data)
+      $http.get(urlBase + '/user/'+ user_id + '/offers').success(function(data){
+        if (data.length != offers.length){
+          for (var item in data) {
+            offers.push(data[item])
+          }
+        }
+      }).error(function(err){
+        console.log("Offers data not found: " + err)
+      })
+    },
+    all: function() {
+      return offers
+    },
+    add: function(listing) {
+      offers.push(listing)
+    },
+    remove: function(listing) {
+      offers.splice(offers.indexof(listing), 1)
+    },
+
+  }
+})
