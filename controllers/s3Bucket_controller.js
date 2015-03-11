9767ea4e0ce4b2a846f304b1be5e1a92dcf37550
yandrew <yandrew@gmail.com>
2015-03-11 09:21:19 -0700

    upload form working

---
+++ controllers/s3Bucket_controller.js
@@ -0,0 +1,8 @@
+app.controller('s3BucketCtrl',function($scope){
+	$scope.creds = {
+		bucket: 'sellme',
+		access_key: 'AKIAJLO3XCGMO2TGYW7Q',
+		secret_key: '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'
+
+	}
+})	
---
+++ s3-bucket/application.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var application = angular.module('angular-s3-upload', [
+    's3Controller',
+    's3Directives'
+]);
